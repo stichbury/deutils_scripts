1fa9825c2ae9eb830177ce538880d4ab58ff6548
Jo Stichbury <jo.stichbury@mac.com>
2019-04-13 17:58:42 +0100

    Update scripts

--- scripts/mobile_evolution.py
+++ scripts/mobile_evolution.py
@@ -35,41 +35,72 @@ Sparta_Core = 'WghtUniversal_Core'
 de16 = de.read_sparta_survey(7)
 de16_meta = de.meta(7)
 
-# Read DE15 data
-#de15 = de.read_sparta_survey(6)
-#de15_meta = de.meta(6)
-
-# Read DE14 data
-#de14_meta = de.meta(5)
-#de14 = de.read_sparta_survey(5)
-
-# Read DE13 data
-#de13 = de.read_sparta_survey(4)
-#de13_meta = de.meta(4)
 
 ##################
 
+#Look at number of mobile devs
 cr2 =  de.dist(de16, de16_meta, 'CR2a', 'WghtUniversal_Core')
+cr2.to_clipboard()
 cr2_pc = de.calc_pct(cr2)
 cr2_pc.to_clipboard()
 
+#Look at number of professional devs
+cr2 =  de.dist(de16, de16_meta, 'CR2b', 'WghtUniversal_Core')
+cr2_pc = de.calc_pct(cr2)
+cr2_pc.to_clipboard()
 
-#Crosstab regions against development area
+# Look at the other development that mobile devs do
+mob_devs = (de16.CR2a_2==1)
+filtered = de.dist(de16[mob_devs], de16_meta, 'CR2a', 'WghtUniversal_Core')
+filtered_pc = de.calc_pct(filtered)
+filtered_pc.to_clipboard()
+
+# Look at the other development that web devs do
+web_devs = (de16.CR2a_1==1)
+filtered = de.dist(de16[web_devs], de16_meta, 'CR2a', 'WghtUniversal_Core')
+filtered_pc = de.calc_pct(filtered)
+filtered_pc.to_clipboard()
+
+# How many professional mobile devs do other stuff?
+
+de16['Prof']  = (de16[['CR2b_2_1']].any(axis=1)).astype(float).replace(0,2)
+cr2pros_only = de.dist(de16[de16.Prof==1], de16_meta, 'CR2b', 'WghtUniversal_Core')
+cr2pros_only_pc = de.calc_pct(cr2pros_only)
+cr2pros_only_pc.to_clipboard()
+
+# CR6: experience levels over survey
+cr6 = de.dist(de16, de16_meta, 'CR6', 'WghtUniversal_Core')
+cr6_pc = de.calc_pct(cr6, pct_type='row')
+cr6_pc.to_clipboard()
+
+# Filter by mobile 
+de16['MOBILE']  = de16[['CR2a_2']].any(axis=1).astype(float).replace(0,np.nan)
+Mob_dev=(de16.MOBILE==1)
+
+#Look at age levels
+# whole dev population
+cr_dev2 = de.dist(de16, de16_meta, 'CR_DEV2', 'WghtUniversal_Core')
+cr_dev2_pc = de.calc_pct(cr_dev2)
+cr_dev2_pc.to_clipboard()
+
+cr_dev2_mobile = de.dist(de16[Mob_dev],de16_meta,'CR_DEV2','WghtUniversal_Core')
+cr_dev2_mobile_pc=de.calc_pct(cr_dev2_mobile)
+cr_dev2_mobile_pc.to_clipboard()
+
+#Crosstab geographical regions against development area
 regions=de.crosstab(de16, de16_meta, 'RegionCode8','CR2a', 'WghtUniversal_Core')
 regions.to_clipboard()
 
-#MOB1: What platforms do you develop  for?
+#MOB1: What hardware platforms do you develop  on?
 mob1 = de.dist(de16, de16_meta, 'MOB1', 'WghtUniversal_Mob')
 mob1pc = de.calc_pct(mob1)
 mob1pc.to_clipboard()
 
 # Mobile professionals only
 de16['Prof']  = (de16[['CR2b_2_1']].any(axis=1)).astype(float).replace(0,2)
-
-pros_only = de.dist(de16[de16.Prof==1], de16_meta, 'MOB1', 'WghtUniversal_Core')
+pros_only = de.dist(de16[de16.Prof==1], de16_meta, 'MOB1', 'WghtUniversal_Mob')
 pros_onlypc = de.calc_pct(pros_only)
 pros_onlypc.to_clipboard()
-
 
 #MOB3: What programming languages?
 mob3 = de.dist(de16, de16_meta, 'MOB3', 'WghtUniversal_Mob')
@@ -78,78 +109,157 @@ mob3pc.to_clipboard()
 
 # Mobile professionals only
 de16['Prof']  = (de16[['CR2b_2_1']].any(axis=1)).astype(float).replace(0,2)
+pros_only = de.dist(de16[de16.Prof==1], de16_meta, 'MOB3', 'WghtUniversal_Mob')
+pros_onlypc = de.calc_pct(pros_only)
+pros_onlypc.to_clipboard()
 
-pros_only = de.dist(de16[de16.Prof==1], de16_meta, 'MOB3', 'WghtUniversal_Core')
+#Filter mobile devs programming language choice by those that use cross platform dev tools
+de16['xplat']  = (de16[['MOB_PA_1']].any(axis=1)).astype(float).replace(0,2)
+mob3_xplat = de.dist(de16[de16.xplat==1], de16_meta, 'MOB3', 'WghtUniversal_Mob')
+mob3_xplatpc = de.calc_pct(mob3_xplat)
+mob3_xplatpc.to_clipboard()
+
+#MOB_PA: What tech do you use
+mobpa = de.dist(de16, de16_meta, 'MOB_PA', 'WghtUniversal_Mob')
+mobpapc = de.calc_pct(mobpa)
+mobpapc.to_clipboard()
+
+#What about professional mobile devs?
+de16['Prof']  = (de16[['CR2b_2_1']].any(axis=1)).astype(float).replace(0,2)
+pros_only = de.dist(de16[de16.Prof==1], de16_meta, 'MOB_PA', 'WghtUniversal_Mob')
+pros_onlypc = de.calc_pct(pros_only)
+pros_onlypc.to_clipboard()
+
+#MOB2: Application category
+mob2 = de.dist(de16, de16_meta, 'MOB2', 'WghtUniversal_Mob')
+mob2pc = de.calc_pct(mob2)
+mob2pc.to_clipboard()
+
+# Mobile professionals only
+de16['Prof']  = (de16[['CR2b_2_1']].any(axis=1)).astype(float).replace(0,2)
+pros_only = de.dist(de16[de16.Prof==1], de16_meta, 'MOB2', 'WghtUniversal_Mob')
+pros_onlypc = de.calc_pct(pros_only)
+pros_onlypc.to_clipboard()
+
+#Filter mobile devs choice of app type by those that use cross platform dev tools
+de16['xplat']  = (de16[['MOB_PA_1']].any(axis=1)).astype(float).replace(0,2)
+mob2_xplat = de.dist(de16[de16.xplat==1], de16_meta, 'MOB2', 'WghtUniversal_Mob')
+mob2_xplatpc = de.calc_pct(mob2_xplat)
+mob2_xplatpc.to_clipboard()
+
+
+#MOB8: Audience sector
+mob8 = de.dist(de16, de16_meta, 'MOB8', 'WghtUniversal_Mob')
+mob8pc = de.calc_pct(mob8)
+mob8pc.to_clipboard()
+
+# Mobile professionals only
+de16['Prof']  = (de16[['CR2b_2_1']].any(axis=1)).astype(float).replace(0,2)
+pros_only = de.dist(de16[de16.Prof==1], de16_meta, 'MOB8', 'WghtUniversal_Mob')
+pros_onlypc = de.calc_pct(pros_only)
+pros_onlypc.to_clipboard()
+
+
+#Look at number of mobile devs with web skills TO DO
+cr2 =  de.dist(de16, de16_meta, 'CR2a', 'WghtUniversal_Core')
+cr2_pc = de.calc_pct(cr2)
+cr2_pc.to_clipboard()
+
+
+#Look at the professional mobile developers working in various sectors 
+de16['Prof']  = (de16[['CR2b_2_1']].any(axis=1)).astype(float).replace(0,2)
+pros_only = de.dist(de16[de16.Prof==1], de16_meta, 'CR_PROF1', 'WghtUniversal_Core')
 pros_onlypc = de.calc_pct(pros_only)
 pros_onlypc.to_clipboard()
 
 
 
-#MS_TECH: Approaches and technologies
+
+
+
+# MOB_POP questions
+pop =  de.dist(de16, de16_meta, 'MOB_POP1', 'WghtUniversal_Mob')
+pop_pc = de.calc_pct(pop)
+pop_pc.to_clipboard()
+
+pop =  de.dist(de16, de16_meta, 'MOB_POP2', 'WghtUniversal_Mob')
+pop_pc = de.calc_pct(pop)
+pop_pc.to_clipboard()
+
+pop =  de.dist(de16, de16_meta, 'MOB_POP3', 'WghtUniversal_Mob')
+pop_pc = de.calc_pct(pop)
+pop_pc.to_clipboard()
+
+# Now the same questions for the professionals only
+de16['Prof']  = (de16[['CR2b_2_1']].any(axis=1)).astype(float).replace(0,2)
+pros_only = de.dist(de16[de16.Prof==1], de16_meta, 'MOB_POP1', 'WghtUniversal_Core')
+pros_onlypc = de.calc_pct(pros_only)
+pros_onlypc.to_clipboard()
+
+de16['Prof']  = (de16[['CR2b_2_1']].any(axis=1)).astype(float).replace(0,2)
+pros_only = de.dist(de16[de16.Prof==1], de16_meta, 'MOB_POP2', 'WghtUniversal_Core')
+pros_onlypc = de.calc_pct(pros_only)
+pros_onlypc.to_clipboard()
+
+de16['Prof']  = (de16[['CR2b_2_1']].any(axis=1)).astype(float).replace(0,2)
+pros_only = de.dist(de16[de16.Prof==1], de16_meta, 'MOB_POP3', 'WghtUniversal_Core')
+pros_onlypc = de.calc_pct(pros_only)
+pros_onlypc.to_clipboard()
+
+# Motivation
+mob4 = de.dist(de16, de16_meta, 'MOB4', 'WghtUniversal_Mob')
+mob4pc = de.calc_pct(mob4)
+mob4pc.to_clipboard()
+
+#Look at the professional mobile developers only
+de16['Prof']  = (de16[['CR2b_2_1']].any(axis=1)).astype(float).replace(0,2)
+pros_only = de.dist(de16[de16.Prof==1], de16_meta, 'MOB4', 'WghtUniversal_Core')
+pros_onlypc = de.calc_pct(pros_only)
+pros_onlypc.to_clipboard()
+
+#Look at ways to make money MOB6 
+de16['Prof']  = (de16[['CR2b_2_1']].any(axis=1)).astype(float).replace(0,2)
+pros_only = de.dist(de16[de16.Prof==1], de16_meta, 'MOB6', 'WghtUniversal_Core')
+pros_onlypc = de.calc_pct(pros_only)
+pros_onlypc.to_clipboard()
+
+#Look at revenue MOB7 
+de16['Prof']  = (de16[['CR2b_2_1']].any(axis=1)).astype(float).replace(0,2)
+pros_only = de.dist(de16[de16.Prof==1], de16_meta, 'MOB7', 'WghtUniversal_Core')
+pros_onlypc = de.calc_pct(pros_only)
+pros_onlypc.to_clipboard()
+
+# Organisation size for pro mobile devs only
+de16['Prof']  = (de16[['CR2b_2_1']].any(axis=1)).astype(float).replace(0,2)
+pros_only = de.dist(de16[de16.Prof==1], de16_meta, 'CR3', 'WghtUniversal_Core')
+pros_onlypc = de.calc_pct(pros_only)
+pros_onlypc.to_clipboard()
+
+
+#Crosstabs
+cross = de.crosstab(de16, de16_meta, 'MOB1', 'MOB_PA', 'WghtUniversal_Mob')
+cross.to_clipboard()
+cross_pc = de.calc_pct(cross)
+cross_pc.to_clipboard()
+
+cross = de.crosstab(de16, de16_meta, 'MOB_PA', 'MOB1', 'WghtUniversal_Mob')
+cross.to_clipboard()
+cross_pc = de.calc_pct(cross)
+cross_pc.to_clipboard()
+
+cross = de.crosstab(de16, de16_meta, 'MOB3', 'MOB_PA', 'WghtUniversal_Mob')
+cross.to_clipboard()
+cross_pc = de.calc_pct(cross)
+cross_pc.to_clipboard()
+
+
+
+
+
+
+#MS_TECH: Approaches and technologies - only asked to desktop, web & games devs, so only really useful
+#for a snapshot
 mstech = de.dist(de16, de16_meta, 'MS_TECH', 'WghtUniversal_Core')
 mstechpc = de.calc_pct(mstech)
 mstechpc.to_clipboard()
 
-# Mobile devs only
-de16['mob']  = (de16[['CR2b_2_1', 'CR2b_2_2', 'CR2b_2_3']].any(axis=1)).astype(float).replace(0,2)
-
-mob_only = de.dist(de16[de16.mob==1], de16_meta, 'MS_TECH', 'WghtUniversal_Core')
-mob_onlypc = de.calc_pct(mob_only)
-mob_onlypc.to_clipboard()
-
-
-# Mobile pros only
-de16['mobpro']  = (de16[['CR2b_2_1']].any(axis=1)).astype(float).replace(0,2)
-
-mobpro_only = de.dist(de16[de16.mobpro==1], de16_meta, 'MS_TECH', 'WghtUniversal_Core')
-mobpro_onlypc = de.calc_pct(mobpro_only)
-mobpro_onlypc.to_clipboard()
-
-
-
-# Now read DE15 data
-de15 = de.read_sparta_survey(6)
-de15_meta = de.meta(6)
-
-
-# CR_DEVCNT2 for DE15
-cr_devcnt2_15 =  de.dist(de15, de15_meta, 'CR_DEVCNT2', 'WghtUniversal_Core')
-cr_devcnt2_15.to_clipboard()
-
-
-
-
-#Age
-#Just women devs (filter)
-dev_female = de.dist(de16[de16['CR_DEV3']==1],de16_meta,'CR_DEV2','WghtUniversal_Core')
-dev_female_pc=de.calc_pct(dev_female)
-dev_female_pc.to_clipboard()
-
-
-#Mob1: Professional Devs Only
-de16['MOB']  = de16[['CR2a_2_1']].any(axis=1).astype(float).replace(0,np.nan)
-pro_mob_dev=de16.MOB==1
-
-mob1PRO = de.dist(de16[pro_mob_dev], de16_meta, 'Mob1', 'WghtUniversal_Mob')
-mob1PROpc = de.calc_pct(mob1PRO)
-mob1PROpc.to_clipboard()
-
-
-#GAM5: Which programming languages do you use to write code that runs on the client side for your games?
-#gam5 = de.dist(de15, de15_meta, 'GAM5', 'WghtUniversal_Game')
-#gam5pc = de.calc_pct(gam5)
-#gam5pc.to_clipboard()
-
-#GAM_PA "What technologies?"
-#gam_pa = de.dist(de15, de15_meta, 'GAM_PA', 'WghtUniversal_Game')
-#gam_pa_pc=de.calc_pct(gam_pa)
-#gam_pa_pc.to_clipboard()
-
-# Filter by games pros
-#de15['GAMES']  = de15[['CR2_8_1', 'CR2_8_2', 'CR2_8_3']].any(axis=1).astype(float).replace(0,np.nan)
-#de15['GAMES']  = de15[['CR2_8_1']].any(axis=1).astype(float).replace(0,np.nan)
-#Game_dev=de15.GAMES==1
-
-#cr6games = de.dist(de15[Game_dev],de15_meta,'CR6','WghtUniversal_Core')
-#cr6g_pc=de.calc_pct(cr6games,pct_type='row')
-#cr6g_pc.to_clipboard()
---
+++ scripts/mobile_evolution_de14.py
@@ -0,0 +1,136 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+# Imports
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb 25 15:54:14 2019
+Granny-Clanger:~ stichbury$ /Users/stichbury/anaconda/envs/py3/bin/spyder ; exit;
+
+@author: stichbury
+"""
+
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+# Imports
+import pandas as pd
+import deutils as de
+import numpy as np
+
+# weights
+Sparta_Core = 'WghtUniversal_Core'
+
+##################
+# Read de14 data
+de14 = de.read_sparta_survey(5)
+de14_meta = de.meta(5)
+##################
+
+
+#Look at number of mobile devs
+cr2 =  de.dist(de14, de14_meta, 'CR2', 'WghtUniversal_Core')
+cr2_pc = de.calc_pct(cr2)
+cr2_pc.to_clipboard()
+
+#Crosstab geographical regions against development area
+regions=de.crosstab(de14, de14_meta, 'RegionCode8','CR2', 'WghtUniversal_Core')
+regions.to_clipboard()
+
+#MOB1: What hardware platforms do you develop  on?
+mob1 = de.dist(de14, de14_meta, 'MOB1', 'WghtUniversal_Mob')
+mob1pc = de.calc_pct(mob1)
+mob1pc.to_clipboard()
+
+# Mobile professionals only
+de14['Prof']  = (de14[['CR2_2_1']].any(axis=1)).astype(float).replace(0,2)
+pros_only = de.dist(de14[de14.Prof==1], de14_meta, 'MOB1', 'WghtUniversal_Core')
+pros_onlypc = de.calc_pct(pros_only)
+pros_onlypc.to_clipboard()
+
+#MOB3: What programming languages?
+mob3 = de.dist(de14, de14_meta, 'MOB3', 'WghtUniversal_Mob')
+mob3pc = de.calc_pct(mob3)
+mob3pc.to_clipboard()
+
+# Mobile professionals only
+de14['Prof']  = (de14[['CR2_2_1']].any(axis=1)).astype(float).replace(0,2)
+pros_only = de.dist(de14[de14.Prof==1], de14_meta, 'MOB3', 'WghtUniversal_Core')
+pros_onlypc = de.calc_pct(pros_only)
+pros_onlypc.to_clipboard()
+
+#Filter mobile devs programming language choice by those that use cross platform dev tools
+de14['xplat']  = (de14[['MOB_PA_1']].any(axis=1)).astype(float).replace(0,2)
+mob3_xplat = de.dist(de14[de14.xplat==1], de14_meta, 'MOB3', 'WghtUniversal_Mob')
+mob3_xplatpc = de.calc_pct(mob3_xplat)
+mob3_xplatpc.to_clipboard()
+
+#MOB_PA: What tech do you use
+mobpa = de.dist(de14, de14_meta, 'MOB_PA', 'WghtUniversal_Mob')
+mobpapc = de.calc_pct(mobpa)
+mobpapc.to_clipboard()
+
+#What about professional mobile devs?
+de14['Prof']  = (de14[['CR2_2_1']].any(axis=1)).astype(float).replace(0,2)
+pros_only = de.dist(de14[de14.Prof==1], de14_meta, 'MOB_PA', 'WghtUniversal_Core')
+pros_onlypc = de.calc_pct(pros_only)
+pros_onlypc.to_clipboard()
+
+#MOB2: Application category
+mob2 = de.dist(de14, de14_meta, 'MOB2', 'WghtUniversal_Mob')
+mob2pc = de.calc_pct(mob2)
+mob2pc.to_clipboard()
+
+# Mobile professionals only
+de14['Prof']  = (de14[['CR2_2_1']].any(axis=1)).astype(float).replace(0,2)
+pros_only = de.dist(de14[de14.Prof==1], de14_meta, 'MOB2', 'WghtUniversal_Core')
+pros_onlypc = de.calc_pct(pros_only)
+pros_onlypc.to_clipboard()
+
+#Filter mobile devs choice of app type by those that use cross platform dev tools
+de14['xplat']  = (de14[['MOB_PA_1']].any(axis=1)).astype(float).replace(0,2)
+mob2_xplat = de.dist(de14[de14.xplat==1], de14_meta, 'MOB2', 'WghtUniversal_Mob')
+mob2_xplatpc = de.calc_pct(mob2_xplat)
+mob2_xplatpc.to_clipboard()
+
+
+#MOB8: Audience sector
+mob8 = de.dist(de14, de14_meta, 'MOB8', 'WghtUniversal_Mob')
+mob8pc = de.calc_pct(mob8)
+mob8pc.to_clipboard()
+
+# Mobile professionals only
+de14['Prof']  = (de14[['CR2_2_1']].any(axis=1)).astype(float).replace(0,2)
+pros_only = de.dist(de14[de14.Prof==1], de14_meta, 'MOB8', 'WghtUniversal_Core')
+pros_onlypc = de.calc_pct(pros_only)
+pros_onlypc.to_clipboard()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#MS_TECH: Approaches and technologies - only asked to desktop, web & games devs, so only really useful
+#for a snapshot
+mstech = de.dist(de14, de14_meta, 'MS_TECH', 'WghtUniversal_Core')
+mstechpc = de.calc_pct(mstech)
+mstechpc.to_clipboard()
+
+
